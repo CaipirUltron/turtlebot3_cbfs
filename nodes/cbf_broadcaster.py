Fixed some ugly stuff in the code. Now CLF/CBF broadcasters are closer to a general formulation.

nodes/cbf_broadcaster.py
@@ -2,38 +2,57 @@
 
 import rospy
 import numpy as np
-from turtlebot_common.Maths import Field
+from turtlebot_common.Maths import Field, Mapping
 from turtlebot3_cbfs.msg import Obstacles, ScalarField
 from geometry_msgs.msg import Pose2D
 
 
-class MergeCBFs:
+class CBFControl:
 
     def __init__(self):
         self.barriers = []
-        self.dim = rospy.get_param('/dim')
-        self.field = Field(self.dim, "quadratic", np.zeros((self.dim, self.dim)), np.zeros(self.dim), 1)
-        self.dim = rospy.get_param('/dim')
+
+        self.model_dim = rospy.get_param('/model_dim')
+        self.model_cbf = Field(self.model_dim, "")
+        self.model_state = np.zeros(self.model_dim)
+
+        self.cbf_dim = rospy.get_param('~dim')
+        self.cbf = Field(self.cbf_dim, "quadratic", np.zeros((self.cbf_dim, self.cbf_dim)), np.zeros(self.cbf_dim), 1)
+        self.cbf_state = np.zeros(self.cbf_dim)
+        self.cbf_jacobian = np.array([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0]])
 
     def obstacles_callback(self, obstacles):
         self.barriers = []
         for i in range(0, obstacles.num):
-            obstacle_position = np.array([obstacles.obstacles[i].pose.x, obstacles.obstacles[i].pose.y, 0])
+            obstacle_position = np.array([obstacles.obstacles[i].pose.x, obstacles.obstacles[i].pose.y])
             axis_x = obstacles.obstacles[i].shape[0]
             axis_y = obstacles.obstacles[i].shape[1]
-            a = np.diag([(1 / np.power(axis_x, 2)), (1 / np.power(axis_y, 2)), 0])
-            b = -2*a.dot(obstacle_position)
+            a = np.diag([(1 / np.power(axis_x, 2)), (1 / np.power(axis_y, 2))])
+            b = -2 * a.dot(obstacle_position)
             c = np.dot(obstacle_position, a.dot(obstacle_position)) - 1
-            cbf = Field(self.dim, "quadratic", a, b, c)
-            self.barriers.append(cbf)
+            cbf_i = Field(self.cbf_dim, "quadratic", a, b, c)
+            self.barriers.append(cbf_i)
 
-    def pose_callback(self, robot_pose):
-        state = np.array([robot_pose.x, robot_pose.y, robot_pose.theta])
-        self.field = Field(self.dim, "quadratic", np.zeros((self.dim, self.dim)), np.zeros(self.dim), 1)
-        self.field.setVar(state)
+    def set_pose(self, data):
+        self.model_state = np.array([data.x, data.y, data.theta])
+        self.compute_transformation()
+        self.merge_CBFs()
+        self.compute_modelCBF()
+
+    def compute_transformation(self):
+        self.cbf_state = self.cbf_jacobian.dot(self.model_state)
+        # self.cbf_jacobian = np.array([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0]])
+
+    def merge_CBFs(self):
+        self.cbf = Field(self.cbf_dim, "quadratic", np.zeros((self.cbf_dim, self.cbf_dim)), np.zeros(self.cbf_dim), 1)
+        self.cbf.computeField(self.cbf_state)
         for i in range(0, len(self.barriers)):
-            self.barriers[i].setVar(state)
-            self.field = self.field * self.barriers[i]
+            self.barriers[i].computeField(self.cbf_state)
+            self.cbf = self.cbf * self.barriers[i]
+
+    def compute_modelCBF(self):
+        self.model_cbf.field = self.cbf.field
+        self.model_cbf.gradient = np.matmul(self.cbf.gradient, self.cbf_jacobian)
 
 
 if __name__ == '__main__':
@@ -44,14 +63,14 @@ if __name__ == '__main__':
         pub_freq = rospy.get_param('~rate')
         rate = rospy.Rate(pub_freq)
 
-        merged_cbf = MergeCBFs()
+        clf_controller = CBFControl()
 
-        obstacle_sub = rospy.Subscriber("obstacles_topic", Obstacles, merged_cbf.obstacles_callback)
-        pose_sub = rospy.Subscriber("turtlebot_pose", Pose2D, merged_cbf.pose_callback)
+        obstacle_sub = rospy.Subscriber("obstacles_topic", Obstacles, clf_controller.obstacles_callback)
+        pose_sub = rospy.Subscriber("turtlebot_pose", Pose2D, clf_controller.set_pose)
         cbf_pub = rospy.Publisher('cbf', ScalarField, queue_size=1)
 
         while not rospy.is_shutdown():
-            cbf_pub.publish(merged_cbf.field.getField())
+            cbf_pub.publish(clf_controller.model_cbf.getField())
             rate.sleep()
 
     except rospy.ROSInterruptException:

nodes/clf_broadcaster.py
@@ -2,7 +2,7 @@
 
 import rospy
 import numpy as np
-from turtlebot_common.Maths import Field
+from turtlebot_common.Maths import Field, Mapping
 from turtlebot3_cbfs.msg import ScalarField
 from geometry_msgs.msg import Pose2D, PointStamped
 
@@ -20,38 +20,46 @@ def rot(th):
 
 class CLFControl:
     def __init__(self):
-        self.dim = rospy.get_param('/dim')
-        self.clf = Field(self.dim, "")
-        self.error_clf = Field(self.dim-1, "quadratic", 0.5 * np.diag(rospy.get_param('~lambdas')), np.zeros(self.dim-1), 0.0)
+        self.model_dim = rospy.get_param('/model_dim')
+        self.model_clf = Field(self.model_dim, "")
+        self.model_state = np.zeros(self.model_dim)
+
+        self.clf_dim = rospy.get_param('~dim')
+        self.clf = Field(self.clf_dim, "quadratic", 0.5 * np.diag(rospy.get_param('~lambdas')), np.zeros(self.clf_dim),
+                         0.0)
+        self.clf_state = np.zeros(self.clf_dim)
+        self.clf_jacobian = np.zeros((self.clf_dim, self.model_dim))
+
         self.epsilon = np.array(rospy.get_param('~epsilon'))
-        self.reference = np.zeros(self.dim-1)
-        self.state = np.zeros(self.dim)
-        self.error = np.zeros(self.dim-1)
-        self.grad_error = np.zeros((self.dim, self.dim-1))
-        self.compute_error()
+        self.reference = np.zeros(self.clf_dim)
+
+        # self.compute_transformation()
 
     def set_pose(self, data):
-        self.state = np.array([data.x, data.y, data.theta])
-        self.compute_error()
+        self.model_state = np.array([data.x, data.y, data.theta])
+        self.compute_transformation()
 
     def set_reference(self, data):
         self.reference = np.array([data.point.x, data.point.y])
         rospy.loginfo("Changing reference...")
-        self.compute_error()
+        self.compute_transformation()
 
-    def compute_error(self):
-        robot_position = self.state[:2]
-        robot_orientation = self.state[2]
+    # PF error transformation
+    def compute_transformation(self):
+        robot_position = self.model_state[:2]
+        robot_orientation = self.model_state[2]
         robot_rotation = rot(robot_orientation)
-        self.error = np.transpose(robot_rotation).dot(robot_position - self.reference) + self.epsilon
-        self.grad_error = np.vstack([robot_rotation, -skew(1).dot(self.error - self.epsilon)])
-        self.compute_CLF()
 
-    def compute_CLF(self):
-        self.error_clf.setVar(self.error)
-        self.clf.field = self.error_clf.field
-        self.clf.gradient = np.matmul(self.grad_error, self.error_clf.gradient)
-        self.clf.hessian = np.zeros((self.dim, self.dim))
+        self.clf_state = np.transpose(robot_rotation).dot(robot_position - self.reference) + self.epsilon
+        self.clf_jacobian = np.concatenate(
+            (np.transpose(robot_rotation), np.transpose([-skew(1).dot(self.clf_state - self.epsilon)])), axis=1)
+
+        self.compute_modelCLF()
+
+    def compute_modelCLF(self):
+        self.clf.computeField(self.clf_state)
+        self.model_clf.field = self.clf.field
+        self.model_clf.gradient = np.matmul(self.clf.gradient, self.clf_jacobian)
 
 
 if __name__ == '__main__':
@@ -69,7 +77,7 @@ if __name__ == '__main__':
         clf_pub = rospy.Publisher('clf', ScalarField, queue_size=1)
 
         while not rospy.is_shutdown():
-            clf_pub.publish(clf_controller.clf.getField())
+            clf_pub.publish(clf_controller.model_clf.getField())
             rate.sleep()
 
     except rospy.ROSInterruptException:

src/turtlebot_common/Maths.py
@@ -20,11 +20,11 @@ class Field:
             self.b = args[1]
             self.c = args[2]
 
-        self.var = np.zeros(dim)
+        self.var = np.zeros(self.dim)
         self.field = 0.0
         self.gradient = np.zeros(self.dim)
         self.hessian = np.zeros([self.dim, self.dim])
-        self.compute_local()
+        self.computeField(self.var)
 
     def __add__(self, other):
         if self.dim != other.dim:
@@ -64,7 +64,7 @@ class Field:
             raise ValueError('Fields computed at different points.')
 
         new_field = Field(self.dim, "")
-        new_field.setVar(self.var)
+        new_field.var = self.var
         new_field.field = self.field * other.field
         new_field.gradient = self.gradient * other.field + other.gradient * self.field
         grad1grad2 = np.outer(self.gradient, other.gradient)
@@ -73,12 +73,8 @@ class Field:
 
         return new_field
 
-    def setVar(self, data):
+    def computeField(self, data):
         self.var = data
-        self.compute_local()
-
-    def compute_local(self):
-        data = self.var
         if self.field_type == "const":
             self.field = self.c
             self.gradient = np.zeros(self.dim)
@@ -113,6 +109,17 @@ class Field:
         self.field = scalar_field.field
         self.gradient = np.array(scalar_field.gradient)
         self.hessian = np.array(scalar_field.hessian).reshape((self.dim, self.dim))
+
+
+class Mapping:
+
+    def __init__(self, input_dim, output_dim):
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+
+        self.var = np.zeros(self.input_dim)
+        self.transform = np.zeros(self.output_dim)
+        self.jacobian = np.zeros((self.output_dim, self.input_dim))
 
 
 class AffineModel: